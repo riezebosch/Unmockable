b4670c0618b17510bc5b1627da2e80e8b90c9727
Manuel Riezebosch <mriezebosch@gmail.com>
2019-11-18 20:11:27 +0100

    feat(argmatcherfactory): use array indexer since there's always a single argument

--- Unmockable.Intercept/Matchers/ArgMatcherFactory.cs
+++ Unmockable.Intercept/Matchers/ArgMatcherFactory.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Linq;
 using System.Linq.Expressions;
 
 namespace Unmockable.Matchers
@@ -11,7 +10,7 @@ namespace Unmockable.Matchers
                 MethodCallExpression call when call.Method.DeclaringType == typeof(Arg) => call.Method.Name switch
                 {
                     nameof(Arg.Ignore) => (IArgumentMatcher)new IgnoreArgument(),
-                    nameof(Arg.Where) => new LambdaArgument(call.Arguments.Single()),
+                    nameof(Arg.Where) => new LambdaArgument(call.Arguments[0]),
                     _ => throw new InvalidOperationException()
                 },
                 _ => null
